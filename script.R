951678981aaf2ec334ea950a7c6fddaf954f78fc
Justina <justina.jemeljanovaite@gmail.com>
2025-05-18 16:08:43 +0200

    Only one news indicator (newspapers) + valgdeltagelse 2022 - alpha 0.5

--- script.R
+++ script.R
@@ -20,10 +20,9 @@ cses_dk <- cses6 %>%
 cses_engagement_dk <- cses_dk %>%
   select(
     interesse = F3001,           # Q1
-    tv = F3002_1,                # Q2a
-    radio = F3002_3,             # Q2b
     avis = F3002_4,              # Q2c
     intern_effektivitet = F3003, #Q3
+    stem2022 = F3010_LH,         # Q12a
     uddannelse = F2003
   )
 
@@ -32,33 +31,36 @@ cses_engagement_dk  <- cses_engagement_dk  |>
   mutate(
     across(c(interesse, intern_effektivitet), ~ na_if(., 7)),
     across(c(interesse, intern_effektivitet), ~ na_if(., 8)),
-    across(c(interesse, tv, avis, intern_effektivitet), ~ na_if(., 97)),
-    across(c(interesse, tv, avis, intern_effektivitet), ~ na_if(., 98)),
+    across(c(avis), ~ na_if(., 97)),
+    across(c(avis), ~ na_if(., 98)),
+    stem2022 = na_if(stem2022, 3),
+    stem2022 = na_if(stem2022, 7),
+    stem2022 = na_if(stem2022, 8)
   )
 
 # Reverse-kod Q1 & Q3
 cses_engagement_dk  <- cses_engagement_dk  |>
   mutate(
     interesse_rev = 5 - interesse,
-    intern_effektivitet_rev = 6 - intern_effektivitet
+    intern_effektivitet_rev = 6 - intern_effektivitet,
+    stem2022_rev = 3 - stem2022
   )
 
 # Skaler alle til 1-4 skala
 cses_engagement_dk <- cses_engagement_dk |>
   mutate(
-    interesse_rev_scaled = scales::rescale(interesse_rev, to = c(1, 4)),
-    intern_effektivitet_rev_scaled = scales::rescale(intern_effektivitet_rev, to = c(1, 4)),
-    tv_scaled = scales::rescale(as.numeric(tv), to = c(1, 4)),
-    radio_scaled = scales::rescale(as.numeric(radio), to = c(1, 4)),
-    avis_scaled = scales::rescale(as.numeric(avis), to = c(1, 4))
+    interesse_rev_scaled = scales::rescale(interesse_rev, to = c(0, 1)),
+    intern_effektivitet_rev_scaled = scales::rescale(intern_effektivitet_rev, to = c(0, 1)),
+    avis_scaled = scales::rescale(as.numeric(avis), to = c(0, 1)),
+    stem2022_rev_scaled = scales::rescale(as.numeric(stem2022_rev), to = c(0, 1))
   )
 
 # Konstruer indeks: gennemsnit hvis højst én NA
 cses_engagement_dk <- cses_engagement_dk |>
   rowwise() |>
   mutate(
-    engagement_rå = mean(c_across(c(interesse_rev_scaled, intern_effektivitet_rev_scaled, tv_scaled, radio_scaled, avis_scaled)), na.rm = TRUE),
-    engagement_mangler = sum(is.na(c_across(c(interesse_rev_scaled, intern_effektivitet_rev_scaled, tv_scaled, radio_scaled, avis_scaled)))),
+    engagement_rå = mean(c_across(c(interesse_rev_scaled, intern_effektivitet_rev_scaled, avis_scaled,  stem2022_rev_scaled)), na.rm = TRUE),
+    engagement_mangler = sum(is.na(c_across(c(interesse_rev_scaled, intern_effektivitet_rev_scaled, avis_scaled,  stem2022_rev_scaled)))),
     engagement_index = ifelse(engagement_mangler <= 1, engagement_rå, NA)
   ) |>
   ungroup()
@@ -70,8 +72,7 @@ print(typetal_engagement)
 # Cronbach's alpha
 psych::alpha(cses_engagement_dk[, c(
   "interesse_rev_scaled",
-  "tv_scaled",
-  "radio_scaled",
   "avis_scaled",
-  "intern_effektivitet_rev_scaled"
+  "intern_effektivitet_rev_scaled",
+  "stem2022_rev_scaled"
 )])
